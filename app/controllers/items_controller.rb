c602224fb9e4ef503570637e4550c7da767c467b
ecmerchant <ecmerchant.contact@gmail.com>
2017-10-02 03:08:42 +0900

    Update devise

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
 
+  require 'csv'
+  before_action :authenticate_user!, only: :get
+
   def get
     res = params[:data]
+    @user = current_user.email
     if res != nil then
+      logger.debug(current_user.email)
       logger.debug("\n<=debug\n")
       result = JSON.parse(res)
 
@@ -10,7 +15,7 @@ class ItemsController < ApplicationController
       i = 0
       while i < result.length
           url = result[i][0]
-          if url != ""  then
+          if url != "" && url != nil  then
             charset = nil
             html = open(url) do |f|
               charset = f.charset # 文字種別を取得
@@ -25,21 +30,28 @@ class ItemsController < ApplicationController
               condition = doc.xpath('//dd[@class="ProductDetail__description"]')[0].text.gsub("\n","")
               condition = condition.slice(1,condition.length)
 
-              restTime = doc.xpath('//dd[@class="Count__number"]')[0].text.gsub("\n","")
-              restTime = restTime.slice(0,restTime.length)
-
               priceType = doc.xpath('//div[@class="Price Price--current"]//dd[@class="Price__value"]')
-              if priceType != nil then
+
+              if priceType[0] != nil then
                 listPrice = priceType[0].text.gsub("\n","")
               else
                 listPrice = ""
               end
+
               priceType = doc.xpath('//div[@class="Price Price--buynow"]//dd[@class="Price__value"]')
-              if priceType != nil then
+              if priceType[0] != nil then
                 binPrice = priceType[0].text.gsub("\n","")
               else
                 binPrice = ""
               end
+
+              bitnum = doc.xpath('//dd[@class="Count__number"]')[0].text
+              bitnum = bitnum.slice(0,bitnum.length-4)
+
+              restTime = doc.xpath('//dd[@class="Count__number"]')[1].text
+              restTime = restTime.slice(0,restTime.length-4)
+              logger.debug(restTime)
+
             else
               title = doc.xpath('//h1[@class="ProductTitle__text"]').text.gsub("\n","")
               auctionID = doc.xpath('//dd[@class="ProductDetail__description"]')[11].text.gsub("\n","")
@@ -66,4 +78,28 @@ class ItemsController < ApplicationController
       render json: res
     end
   end
+
+  def upload
+    csv_data = CSV.read('app/others/csv/Flat.File.Toys.jp.csv', headers: true)
+    #logger.debug(csv_data)
+    #header = csv_data
+    render json: csv_data
+  end
+
+  def set
+    res = params[:data]
+    logger.debug("\n<=debug\n")
+    if res != nil then
+      result = JSON.parse(res)
+      logger.debug(result)
+      res = []
+      i = 0
+      logger.debug("\ndebug=>\n")
+    end
+  end
+
+  def login_check
+    @user = current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   get 'items', to: 'items#get'
   post 'items/get'
 
+  post 'items/upload'
+
+  get 'items/set'
+  post 'items/set'
+
+  get 'items/login_check'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
